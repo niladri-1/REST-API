4260c52a2f58c11d714f4392904757cf487044d0
Niladri Chatterjee <code.niladri+github@gmail.com>
2026-02-10 12:34:23 +0530

    updated Home

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
 
-app.get('/', (req,res) => {
-	console.log("[Test!]");
-	res.send("Hello from Homepage.")
+app.get('/', (req, res) => {
+	console.log("[Serving API Documentation]");
+	res.sendFile(path.join(__dirname, 'home.html'));
 })
 
 app.listen(PORT, () =>
